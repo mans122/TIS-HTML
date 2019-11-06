a24b75c3eff09c2615cd8276bc9a9861af28e23f
mans122 <gksqufdhkdwk2@gmail.com>
2019-11-06 14:37:46 +0900

    d

--- JAVASCRIPT/ajaxDB.php
+++ JAVASCRIPT/ajaxDB.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-type:text/xml; charset=utf-8"); 
+header("Content-type:text/html; charset=utf-8"); 
 
 $mysql_hostname = 'localhost';
 $mysql_username = 'mans122';
--- JAVASCRIPT/ajaxDB_2.php
+++ JAVASCRIPT/ajaxDB_2.php
@@ -1,5 +1,6 @@
 <?
-header("content-type:text/html; charset=utf-8"); 
+header("access-control-allow-origin: *"); 
+header("content-type:text/json; charset=utf-8"); 
 $mysql_hostname = 'localhost';//localhost
 $mysql_username = 'mans122';//root
 $mysql_password = 'Qwas7946!';
@@ -12,12 +13,23 @@ $mysql_col3 = 'age';
 $sql = "SELECT * FROM ".$mysql_table;
 $connect = mysqli_connect($mysql_hostname, $mysql_username, $mysql_password, $mysql_database);
 mysqli_select_db($connect, $mysql_database) or die('DB 선택 실패');
+$query = 'select name,price from products;';
 $result = mysqli_query($connect, $sql);
-while($info=mysqli_fetch_array($result)){
-    echo $info[$mysql_col1]." | ";
-    echo $info[$mysql_col2]."<br/>\n";
-    // echo $info[$mysql_col2]." | ";
-    // echo $info[$mysql_col3]."<br/>\n";
+$output='';
+while($row = mysqli_fetch_array($result))
+{	
+    if ($output!="")
+    {
+        $output.= ",";//콤마붙이기
+    }
+    // $output.='{"zipcode":"'.$row['zipcode'].'"}';  
+    $output.='{"name":"'.$row['name'].'",';
+    $output.='"price":"'.$row['price'].'"}';  
 }
+$output='['.$output.']';
+
+echo $output;
+
+
 mysqli_close($connect);
 ?>
